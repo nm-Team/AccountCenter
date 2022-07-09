6d0f28a27ed7fa21964dbf20fa18044e36cd9ae0
CodyNotFound <codynotfound@outlook.com>
2022-07-09 12:23:00 +0800

    feat: token model

--- backend/src/model/token.ts
+++ backend/src/model/token.ts
@@ -1,7 +1,6 @@
-import { Token } from 'graphql';
-import { randomStr } from '../utils';
 import bus from './bus';
 import db from './mongo';
+import { Collection, ObjectId, Filter } from 'mongodb';
 
 enum TokenType {
     SESSION,
@@ -10,27 +9,60 @@ enum TokenType {
 }
 
 class TokenModel {
-    static coll = db.collection('token');
+    static coll: Collection;
 
-    async add(tokenType: TokenType, expire: number, data: any): Promise<string> {
-        console.log(db);
+    add(tokenType: TokenType, expire: number, data: Object): string {
         const time = new Date();
-        const id = randomStr();
-        await TokenModel.coll.insertOne({
-            ...data,
-            _id: id,
+        const _id = new ObjectId();
+        const id = _id.toString();
+        TokenModel.coll.insertOne({
+            _id,
             tokenType,
             createAt: time,
             updateAt: time,
             expireAt: new Date(time.getTime() + expire * 1000),
+            ...data,
         });
-        bus.emit('token/add', tokenType, id);
+        bus.emit('token/add', id, tokenType);
         return id;
     }
-}
 
-bus.once('app/start', () => {
+    get(tokenId: string, tokenType: TokenType): Promise<Object | null> {
+        bus.emit('token/get', tokenId, tokenType);
+        return TokenModel.coll.findOne({
+            id: new ObjectId(tokenId),
+            tokenType,
+        });
+    }
+
+    query(tokenType: TokenType, query: Filter<Object>) {
+        bus.emit('token/query', tokenType, query);
+        return TokenModel.coll.find({
+            tokenType,
+            ...query,
+        }).toArray();
+    }
+
+    async update(tokenId: string, tokenType: TokenType, expire: number, data: Object) {
+        bus.emit('token/update', tokenId, tokenType);
+        const time = new Date();
+        const res = await TokenModel.coll.findOneAndUpdate(
+            { id: new ObjectId(tokenId), tokenType },
+            {
+                $set: {
+                    ...data,
+                    updateAt: time,
+                    expireAt: new Date(time.getTime() + expire * 1000),
+                },
+            },
+        );
+        return res.value;
+    }
+}
+bus.once('mongo/connected', () => {
+    TokenModel.coll = db.collection('token');
     TokenModel.coll.createIndex({ "expireAt": 1 }, { expireAfterSeconds: 0 });
+    bus.emit('token/inited');
 });
 
 export { TokenType, TokenModel };
