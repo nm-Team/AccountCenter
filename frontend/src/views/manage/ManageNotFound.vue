a6dc9e3afc77f566e1550c835fc9af0c68e9fe8b
CodyNotFound <codynotfound@outlook.com>
2022-07-21 11:20:44 +0800

    fix(ui): path require

--- frontend/src/views/manage/ManageNotFound.vue
+++ frontend/src/views/manage/ManageNotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="not-found" src="../../../public/notfound.png" :alt="$t('manage.not_found_alt')" />
+    <img class="not-found" src="/public/notfound.png" :alt="$t('manage.not_found_alt')" />
 </template>
 <script>
 export default {
