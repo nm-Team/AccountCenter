402bd779a10001800ebf39888191091e992b0d4b
CodyNotFound <codynotfound@outlook.com>
2022-07-30 23:03:29 +0800

    feat(mail): example config

--- packages/mail/src/config.example.ts
+++ packages/mail/src/config.example.ts
@@ -2,5 +2,11 @@ const mongo = {
     url: 'mongodb://localhost:27017/',
     db: 'accountcenter',
 };
+const mail = {
+    host: '',
+    port: 25,
+    user: '',
+    pass: '',
+};
 
-export { mongo };
+export { mongo, mail };
