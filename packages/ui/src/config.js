564ffafc742d80d2fe78d8f8df0c39c683b48692
agoudbg <agoudbg@gmail.com>
2023-07-17 16:44:18 +0800

    fix: env variables prefixes

--- packages/ui/src/config.js
+++ packages/ui/src/config.js
@@ -1,4 +1,4 @@
 export default {
-    backEnd: import.meta.env.BACKEND ?? 'http://localhost:4000/graphql',
-    oauthBackEnd: import.meta.env.OAUTH_BACKEND ?? 'http://localhost:4000/oauth',
+    backEnd: import.meta.env.VITE_BACKEND ?? 'http://localhost:4000/graphql',
+    oauthBackEnd: import.meta.env.VITE_OAUTH_BACKEND ?? 'http://localhost:4000/oauth',
 };
